00c48df0f5a135adc2db0e99c6628427fea046a6
Daniel Alencar <alencar.png@gmail.com>
2023-11-30 20:59:35 -0300

    fix: added user and studio to the comments.module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,9 +3,11 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from 'src/comments/entities/comment.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Studio } from 'src/studios/entities/studio.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [TypeOrmModule.forFeature([Comment, User, Studio])],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
